11b85a529c674da19c8ddd9970e4dba9d0d88f38
baardl <bard.lind@gmail.com>
2019-12-01 20:55:33 +0100

    Howto

--- build_enums/README.md
+++ build_enums/README.md
@@ -1,5 +1,12 @@
 # Build java enums from Bacnet spec
 
-1. Add bacnet spec text to (spec_content.txt)
+1. Add bacnet spec text to [spec_content.txt](spec_content.txt)
 2. `python3 bacnetSpec2enum.py`
-3. Read the result in (content_camelcase.txt)
+3. Read the result in [content_camelcase.txt](content_camelcase.txt)
+
+## Tip
+
+1. Copy from .pdf to Google Sheets
+2. Adjust columns of name and (number)
+3. Copy to [spec_content.txt](spec_content.txt)
+4. Run as described above
--- build_enums/bacnetSpec2enum.py
+++ build_enums/bacnetSpec2enum.py
@@ -2,18 +2,19 @@ import re
 str = open('spec_content.txt','r').read()
 replaced = re.sub(r"\n \(", "(", str)
 print(replaced)
-f = open("content_replaced.txt", "wx")
+f = open("content_replaced.txt", "w")
 f.write(replaced)
 f.close()
 
-camelCase = open('content_camelcase.txt','wx')
+camelCase = open('content_camelcase.txt','w')
 with open('content_replaced.txt', 'r') as reader:
     # reader = open('spec_content.txt')
     try:
         line = reader.readline()
         while line != '':  # The EOF char is an empty string
             line = line.title()
-            line = line.replace('-','');
+            line = line.replace('-','')
+            line = line.replace('\t', '')
             # line = line.replace('(', '(\'')
             # line = line.replace(')', '\')')
 
